f94a09b32e9e3b69a85bb64621cfe930bf370dd1
KoptevAnton <koptev110593@gmail.com>
2025-02-18 09:39:10 +0100

    fix: invert equality check in authentication logic

--- src/services/auth.js
+++ src/services/auth.js
@@ -24,7 +24,7 @@ export const loginUser = async (payload) => {
 
   const isEqual = await bcrypt.compare(payload.password, user.password);
 
-  if (isEqual) throw createHttpError(401, 'Unauthorized');
+  if (!isEqual) throw createHttpError(401, 'Unauthorized');
 
   await SessionsCollection.deleteOne({ userId: user._id });
 
