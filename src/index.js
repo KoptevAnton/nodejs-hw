c6559f9f363ed04e94177a28eea6c08a017d36c7
KoptevAnton <koptev110593@gmail.com>
2025-02-23 20:37:07 +0100

    feat: add password reset via email and contact image upload to cloud storage

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
+import { TEMP_UPLOAD_DIR } from './constants/contacts.js';
 import initMongoConnection from './db/initMongoConnection.js';
 import setupServer from './server.js';
+import { createDirIfNotExist } from './utils/createDirIfNotExists.js';
 
 const bootstrap = async () => {
   await initMongoConnection();
+  await createDirIfNotExist(TEMP_UPLOAD_DIR);
   setupServer();
 };
 
